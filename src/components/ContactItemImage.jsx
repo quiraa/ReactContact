d3690b5f3fcfedc2d1348131f6908922c2b01694
daichi <aryadavicenna@gmail.com>
2024-01-23 12:37:12 +0700

    first commit

---
+++ src/components/ContactItemImage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ContactItemImage({ imageUrl }){
+    return (
+        <div className="contact-item__image">
+            <img src={imageUrl} alt="contact-avatar" width={200} />
+        </div>
+    )
+}
+
+export default ContactItemImage
+
---
+++ src/utils/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Falisha',
+            tag: 'falishaaii',
+            imageUrl: 'images/falisha.jpg'
+        },
+        {
+            id: 2,
+            name: 'Arya',
+            tag: 'aryow',
+            imageUrl: 'images/arya.jpg'
+        },
+        {
+            id: 3,
+            name: 'Joseph',
+            tag: 'jossph',
+            imageUrl: 'images/joseph.jpg'
+        }
+    ];
+}
+
+export { getData }
